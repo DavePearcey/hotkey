7c12be3ddb4b826857507cd3402e646148385762
Demerol <zach@z-dxn.com>
2018-07-01 15:33:06 -0400

    fixing upload more acl

--- upload.sh
+++ upload.sh
@@ -2,4 +2,4 @@
 
 set -e
 
-aws s3 cp hotkey-only.js s3://rwk-assets/hotkey-only.js --cache-control "max-age=0" --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers
+aws s3 cp hotkey-only.js s3://rwk-assets/hotkey-only.js --cache-control "max-age=0" --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers --acl public-read

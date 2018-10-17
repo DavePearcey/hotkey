29887f91a27886cc391a390cd0a4bfe0cc8f3f6c
Zachary Dixon <zach@z-dxn.com>
2018-10-17 18:33:57 -0400

    fuck

--- hotkey-only.js
+++ hotkey-only.js
@@ -14,7 +14,7 @@ if (top.login.toLowerCase() == "anubis") {
 }
 
 var p = {
-  version: "Version: 12",
+  version: "12",
   pwps: [0, 0, 0, 0],
   current_inventory: [],
   current_inventory_length: 0,

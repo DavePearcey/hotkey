3b99bbb6282c67d56a94f2b84f6cb3da837aaaf8
zdxn <zach@z-dxn.com>
2018-10-30 10:05:26 -0400

    adding enhancements

--- upload.sh
+++ upload.sh
@@ -5,7 +5,9 @@ set -e
 if [[ "$OS" != "Windows_NT" ]]; then
   aws s3 cp hotkey-only.js s3://rwk-assets/hotkey-only.js --cache-control "max-age=0" --acl public-read --profile personal
   aws s3 cp chatmod.js s3://rwk-assets/chatmod.js --cache-control "max-age=0" --acl public-read --profile personal
+  aws s3 cp chatmod.js s3://rwk-assets/enhancements.js --cache-control "max-age=0" --acl public-read --profile personal
 else
   aws s3 cp hotkey-only.js s3://rwk-assets/hotkey-only.js --cache-control "max-age=0" --acl public-read
   aws s3 cp chatmod.js s3://rwk-assets/chatmod.js --cache-control "max-age=0" --acl public-read
+  aws s3 cp chatmod.js s3://rwk-assets/enhancements.js --cache-control "max-age=0" --acl public-read 
 fi

12d7d8307cd2463cd0c7fcd8df5553f4125f6e10
Zachary Dixon <zach@z-dxn.com>
2018-11-03 12:51:08 -0400

    removing birthday

--- chatmod.js
+++ chatmod.js
@@ -1,22 +1,5 @@
 var chat_colors = ['6666FF', 'BBBBFF', 'C8C8C8', 'FFBBBB', 'FF6666', '969696', 'CC00CC', '4444FF', 'FF2222'];
-var birthday_names = [ {"name": "Coal Chamber", "color": "00ff0f"},
-{"name": "Fails Like Carrots", "color": "00ff0f"},
-{"name": "Moonlight", "color": "00ff0f"},
-{"name": "Verysmallchurches0", "color": "00ff0f"},
-{"name": "Jjjason", "color": "00ff0f"},
-{"name": "Rainbow Six", "color": "00ff0f"},
-{"name": "Hate Machine", "color": "00ff0f"},
-{"name": "Aeryn Sun", "color": "00ff0f"},
-{"name": "Mojo Jojo", "color": "00ff0f"},
-{"name": "Jiggery", "color": "00ff0f"},
-{"name": "Race War Kingdoms", "color": "00ff0f"},
-{"name": ">Laughing At Noobs<", "color": "00ff0f"},
-{"name": "Scipio", "color": "00ff0f"},
-{"name": "Team Mexico", "color": "00ff0f"},
-{"name": "Captain America", "color": "00ff0f"},
-{"name": "Team Nafta", "color": "00ff0f"},
-{"name": "Korruptionkitten", "color": "00ff0f"},
-{"name": "Spencer", "color": "00ff0f"}];
+var birthday_names = [];
 var donor_names = [{
     "name": ">Anubis<",
     "color": "FFA500",

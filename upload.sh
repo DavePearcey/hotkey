c976d85ef59f0fec4d70cbc0a487f1a4369b38dd
Zachary Dixon <zach@z-dxn.com>
2019-01-04 15:18:12 -0500

    Updating and pushing to s3

--- upload.sh
+++ upload.sh
@@ -17,5 +17,5 @@ else
   aws s3 cp hotkey-only.js s3://rwk-assets/hotkey-only.js --cache-control "max-age=0" --acl public-read
   aws s3 cp chatmod.js s3://rwk-assets/chatmod.js --cache-control "max-age=0" --acl public-read
   aws s3 cp chatmod.js s3://rwk-assets/enhancements.js --cache-control "max-age=0" --acl public-read 
-fi
+fi   
 curl 'http://rwk2.racewarkingdoms.com/cgi-bin/rwk.cgi' --compressed -H 'Referer: http://rwk2.racewarkingdoms.com/cgi-bin/rwk.cgi' -H 'Content-type: application/x-www-form-urlencoded' -H 'Cookie: login=Auctioneer; password=; timing=1262813443.789997' -H 'Connection: keep-alive' --data "login=Auctioneer&timing=undef1262813443.789997&password=testing&action=chat&otherb=none&target=$1&other=0&"

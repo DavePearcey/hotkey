238baf1ae84ea565c0f6a3d413792f8ee01f4713
Zachary Dixon <zach@z-dxn.com>
2019-01-21 15:57:22 -0500

    Updating and pushing to s3

--- hotkey-only.js
+++ hotkey-only.js
@@ -588,7 +588,7 @@ setTimeout(function () {
   genfull('chat', '/dis', 0);
   curInv();
   domes('Addon Version:' + p.version + ' Loaded successfully.');
-  domes("You can now ash items by clicking on the Ash: ## Located beside the inventory counts on player stats. Use this at your own risk, and at your items risk.");
+  domes("You can now ash items by clicking on the Ash Junk items in the addon bar. Use this at your own risk, and at your items risk.");
   upbuttons();
   maxInv();
 }, 500);

79bfc017600798e932b0f3031d73cec5d366888a
zdxn <zach@z-dxn.com>
2019-01-25 09:39:48 -0500

    Updating and pushing to s3

---
+++ map_finder.js
@@ -0,0 +1,88 @@
+  if (document.title.toLowerCase().indexOf('map') > 1) {
+    var img = document.getElementsByTagName('img')[9];
+    var bs = document.createElement("span");
+    document.body.appendChild(bs);
+    document.body.lastChild.id = "span";
+    if (document.location.href.match(/(\/(mhev|msky|msur|mdun|mhel|mfor).htm)/gi)) {
+      span.outerHTML = "<center><span id=span> </span> <br/> <a href='javascript:yesterday_Map();'>Yesterdays Map</a> </center>";
+    } else {
+      span.outerHTML = "<center><span id=span> </span> <br/> <a href='javascript:today_Map();'>Todays Map</a> </center>";
+    }
+    document.getElementsByTagName('center')[1].style.display = 'none';
+    for (let x = 0; x < document.getElementsByTagName('p').length; x++) {
+      document.getElementsByTagName('p')[x].style.display = 'none';
+    }
+
+    img.height = 900;
+    img.width = 900;
+
+    for (let x = 0; x < document.getElementsByTagName('img').length; x++) {
+      if (x != 9) {
+        document.getElementsByTagName('img')[x].style.display = "none";
+      }
+    }
+
+    img.onmousemove = function (event) {
+      let pos_x = event.offsetX ?
+        (event.offsetX) :
+        event.pageX - document.getElementsByTagName("img")[9].offsetLeft;
+      let pos_y = event.offsetY ?
+        (event.offsetY) :
+        event.pageY - document.getElementsByTagName("img")[9].offsetTop;
+      map_data(pos_x, pos_y);
+    }
+  }
+
+  var links = document.createElement("links");
+  document.body.appendChild(links);
+  document.body.lastChild.id = "links";
+  if (document.location.href.match(/(\/(mhev|msky|msur|mdun|mhel|mfor).htm)/gi)) {
+    document.getElementById('links').innerHTML = `<center><a href='../mhev.htm'>Hev</a> <a href='../msky.htm'>Sky</a>  <a href='../msur.htm'>Sur</a>  <a href='../mdun.htm'>Dun</a>  <a href='../mhel.htm'>Hel</a>  <a href = '../mfor.htm' > For</a></center>`;
+  } else {
+    document.getElementById('links').innerHTML = `<center><a href='../yhev.htm'>Hev</a> <a href='../ysky.htm'>Sky</a>  <a href='../ysur.htm'>Sur</a>  <a href='../ydun.htm'>Dun</a>  <a href='../yhel.htm'>Hel</a>  <a href='../yfor.htm'>For</a></center>`;
+  }
+
+
+  function map_data(px, py) {
+    let img = document.getElementsByTagName('img')[9];
+    let canvas = document.createElement('canvas');
+    canvas.width = 900;
+    canvas.height = 900;
+    canvas.getContext('2d').drawImage(img, 0, 0, 900, 900);
+    let data = canvas.getContext('2d').getImageData(px, py, 1, 1).data;
+    //console.log("x: " + px + " y: " + py + " Color: " + data)
+    document.getElementById("span").innerHTML = "X: " + Math.floor((px / 3)) + " Y: " + Math.floor(300 - (py / 3)) + `<br> Color Code: ${data[0]} ${data[1]} ${data[2]}`;
+  }
+
+  var mapname = document.title.substring(0, 3).toLowerCase();
+  var map = "";
+  (() => {
+    switch (mapname) {
+      case "hea":
+        map = "hev";
+        break;
+      case "hel":
+        map = "hel";
+        break;
+      case "sur":
+        map = "sur";
+        break;
+      case "sky":
+        map = "sky";
+        break;
+      case "dun":
+        map = "dun";
+        break;
+      case "for":
+        map = "for";
+        break;
+    }
+  })();
+
+  function today_Map() {
+    document.location.pathname = '/m' + map + ".htm";
+  }
+
+  function yesterday_Map() {
+    document.location.pathname = '/y' + map + ".htm";
+  }
--- upload.sh
+++ upload.sh
@@ -16,11 +16,11 @@ MESSAGE="login=Auctioneer&timing=undef1262813443.789997&password=testing&action=
 if [[ "$OS" != "Windows_NT" ]]; then
   aws s3 cp hotkey-only.js s3://rwk-assets/hotkey-only.js --cache-control "max-age=0" --acl public-read --profile personal
   aws s3 cp chatmod.js s3://rwk-assets/chatmod.js --cache-control "max-age=0" --acl public-read --profile personal
-  aws s3 cp chatmod.js s3://rwk-assets/enhancements.js --cache-control "max-age=0" --acl public-read --profile personal
+  aws s3 cp chatmod.js s3://rwk-assets/map_finder.js --cache-control "max-age=0" --acl public-read --profile personal
 else
   aws s3 cp hotkey-only.js s3://rwk-assets/hotkey-only.js --cache-control "max-age=0" --acl public-read
   aws s3 cp chatmod.js s3://rwk-assets/chatmod.js --cache-control "max-age=0" --acl public-read
-  aws s3 cp chatmod.js s3://rwk-assets/enhancements.js --cache-control "max-age=0" --acl public-read 
+  aws s3 cp chatmod.js s3://rwk-assets/map_finder.js --cache-control "max-age=0" --acl public-read 
 fi   
 curl 'http://rwk2.racewarkingdoms.com/cgi-bin/rwk.cgi' --compressed -H 'Referer: http://rwk2.racewarkingdoms.com/cgi-bin/rwk.cgi' -H 'Content-type: application/x-www-form-urlencoded' -H 'Cookie: login=Auctioneer; password=; timing=1262813443.789997' -H 'Connection: keep-alive' --data "$MESSAGE"
 curl 'http://rwk1.racewarkingdoms.com/cgi-bin/rwk.cgi' --compressed -H 'Referer: http://rwk1.racewarkingdoms.com/cgi-bin/rwk.cgi' -H 'Content-type: application/x-www-form-urlencoded' -H 'Cookie: login=Auctioneer; password=; timing=1262813443.789997' -H 'Connection: keep-alive' --data "$MESSAGE"

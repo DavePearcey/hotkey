c05ddcf2249937466df24025654bc722ddc73140
zdxn <zach@z-dxn.com>
2019-02-08 14:13:06 -0500

    ofc it had to have a typo in it.

--- hotkey-only.js
+++ hotkey-only.js
@@ -3,4 +3,4 @@
 (function () {
   'use strict';
 }());
-domes(`<font size="4" color="yellow"> Addon has been disabled due to changes. Please check out the forums to suggest updates. <br> I appreciate everyone of you so much for using the addon and allowing me to become a better developer in the process. I hope I've reignited your passion in rwk and hope for multiple updates to come in the future to support the player base. Thank you again - Anubis`);
+domes(`<font size="4" color="yellow"> Addon has been disabled due to changes. Please check out the forums to suggest updates. <br> I appreciate everyone of you so much for using the addon and allowing me to become a better developer in the process. I hope I've reignited your passion in rwk and hope for multiple updates to come in the future to support the player base. Thank you again - Anubis</font>`);
